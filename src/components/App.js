22e6f89e397c8916cbe14a60516fa0136c9e8dfb
kikoro86us <kikoro86us@gmail.com>
2017-09-26 15:31:40 -0600

    Auth0&SearchPage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import Landing from './Landing'
+import SearchPage from './SearchPage'
 import '../sass/main.css'
+import { Switch, Route } from 'react-router-dom'
 
 export default class App extends Component {
 	render(){
 		return (
 			<div className='App'>
-				<Landing />
+				<Switch>
+				<Route component= {Landing} exact path='/'/>
+				<Route component= {SearchPage} path='/search'/>
+				</Switch>
 			</div>
 		)
 	}
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import bgvid from '../videos/landingvideo - converted.mp4'
 import logo from '../img/Logo.png'
-// import signin from '../img/SignInBotton.png'
+// import axios from 'axios'
 
 class Landing extends Component {
+    constructor(){
+        super();
+        this.state={
+             
+      }
+      this.handleClick=this.handleClick.bind(this)
+    }
+
+
+handleClick(){
+    // axios.get('/auth')
+}
     render() {
         return (
             <div className='landing'>
                 <div className='logo'><img src={logo} alt=""/></div>
+                
+                <a href='http://localhost:3005/auth'>
                 <div className='signin' onClick={this.handleClick}><p id='signintext'>SIGN IN</p></div>
+                </a>
+
                 <video  id='landingvid' autoPlay muted loop className="css-background-video" data-state="play">
                      <source src={bgvid} type="video/mp4"/>
                  </video>
-                 
-                 {/* <video id='landingvid' autoplay muted loop data-state="play">
-			        <source src={bgvid} type="video/mp4" />
-			        <source src={bgvid} type="video/webm" />
-		        </video> */}
 
             </div>
         );
