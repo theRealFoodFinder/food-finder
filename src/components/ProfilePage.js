01078f7e093293cbdb3a1dbd5b9d5aaf46b5f6ad
FreeFlight-Code <davidfisc@hotmail.com>
2017-09-27 20:15:10 -0600

    array for searchmodal 80%

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfilePage extends Component {
+    render() {
+        return (
+            <div className='profilePageContainer'>
+                Profile Page
+            </div>
+        );
+    }
+}
+
+export default ProfilePage;
