1d692ec0ad46bd0c52b3cbd41f2a0129749d94b5
kikoro86us <kikoro86us@gmail.com>
2017-10-02 13:30:48 -0600

    toworkonthesamepage

--- src/components/Details.js
+++ src/components/Details.js
@@ -12,6 +12,7 @@ class Details extends Component {
 
 
 
+
 componentWillMount() {
     console.log(this.props.recipe[0], 'props on detailedpage')
     this.setState({
@@ -19,45 +20,35 @@ componentWillMount() {
     })
 }
     render() {
+        let ingredients = this.state.results[0].ingredients
+
+       const ingredientsMap = ingredients.map(( ingredient,i ) => {
+
+           return <div key={i} className='oneingredient'> •&nbsp;<div className='unit'>
+{ingredient.DisplayQuantity} {ingredient.Unit} </div> &nbsp; {ingredient.Name} </div>
+        })
 
 
-
-//         let ingredients = this.state.results[0].ingredients
-
-//         const ingredientsMap = ingredients.map(( ingredient,i ) => {
-
-//             return <div key={i} className='oneingredient'> •&nbsp;<div className='unit'>
-// {ingredient.DisplayQuantity} {ingredient.Unit} </div> &nbsp; {ingredient.Name} </div>
-//         })
-
-
-//         let titles = this.state.results[0].title
-
-
-
-
-
-        let titles = this.state.results[0].title
+       let titles = this.state.results[0].title
         return (
 
-            <div className='detailsContainer'>
-                <div className='allappbarcomponents'>
-            <AppBar />
-          </div>
+           <div className='detailsContainer'>
+               <div className='allappbarcomponents'>
+                    <AppBar />
+                </div>
 
-            <a href='#/results'><div className="detailsson">
-                    {/* <img src={this.state.results[0].hero_photo_url} alt="photourl"/> */}
+           <a href='#/results'><div className="detailsson">
+                    <img src={this.state.results[0].hero_photo_url} alt="photourl"/>
                     <div className='titlebackground'>
-                        {/* <div id='foodtitle'>{titles}</div> */}
+                        <div id='foodtitle'>{titles}</div>
                     </div>
                     <div className="detailsson" id='seconddetailsson'>
                         <p id='detailsingredientstitle'>Ingredients</p>
                         <div id='ingredientsmap'>{ingredientsMap}</div>
-                        <div className='detailquestion'> 
+                        <div className='detailquestion'>
                         <Link to='/add'><button> Add these to your shopping list?? </button></Link>
                         </div>
                     </div>
-
                 </div></a>
             </div>
         );
@@ -65,6 +56,3 @@ componentWillMount() {
 }
 
 export default Details;
-
-
-
