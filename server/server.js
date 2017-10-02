a4e81ef425b2f5f29cf7bf7f5c094e8360bac167
robinsonnathan <robinsonnathan1289@gmail.com>
2017-10-02 16:56:47 -0600

    added some documentation in server.js with usable endpoints

--- server/server.js
+++ server/server.js
@@ -1,3 +1,44 @@
+//Hello Front End Peeps
+/* 
+
+- GET - 
+/auth - Redirects to a login page
+
+/auth/logout - Logs the user out
+
+/api/getprofile - Returns all user data on session
+
+/api/getPreferences - Returns user preferences from the user table
+
+/api/favoriteRecipe/:id' - add a specific recipe id to your favorites list
+
+/api/getShoppingList - Returns the list from the shopping_lists table
+
+/api/getRecipe -  /api/getRecipe - POST - Returns a list of recipes based on the ingredients the user has entered, and the items that have been blacklisted. 
+    Client should send an object with a .ingredients property {"ingredients": ['chicken', 'carrot', 'cheese']} and a .nutrition_info object 
+    (accepts `x +`, `x - y`, or `< y` values, where x/y are min/max numbers respectively) -- { "calories": "1000+", "carbs": "<50", "protein": "10-70" } 
+
+
+
+
+- POST -
+
+/api/blacklist - Accepts an object with type: add/remove, and a string of ingredients separated by a comma.  Ex: {type: add, ingredients: "list, of, ingredients")
+
+/api/postShoppingList - Accepts an object with key value pair, ingredient: true/false.  True values get put on shopping list.  False go to the pantry in the users table.  Ex: {chicken: true, cheese: false}
+
+/api/updateShoppingList - Accepts an object. 'items' as the key, and a string of comma separated items to replace the current shopping list in shopping_lists.  Ex: {items: "chicken, cheese"}
+
+/api/appendShoppingList - Accepts an object. 'items' as the key, and a string of comma separated items to append to the current shopping list
+
+
+
+
+
+*/
+
+
+
 const express = require('express'),
 			session = require('express-session'),
 			bodyParser = require('body-parser'),
@@ -503,37 +544,38 @@ app.post('/api/getRecipe', (req,res) => {
         })
 
 
-        app.put('/api/addToBlacklist', (req, res)=> {
-            let {items} = req.body
-            app.get('db').get_blacklist([req.user.id]).then((response)=>{
-                 response = response[0]
-                if (response){
-                    response += ',' + items
-                    app.get('db').update_blacklist([response]).then((response)=>{
-                        res.status(200).send(response)
-                    })
-                }
-            })
-        })
+        // app.put('/api/addToBlacklist', (req, res)=> {
+        //     let {items} = req.body
+        //     app.get('db').get_blacklist([req.user.id]).then((response)=>{
+        //          response = response[0]
+        //         if (response){
+        //             response += ',' + items
+        //             app.get('db').update_blacklist([response]).then((response)=>{
+        //                 res.status(200).send(response)
+        //             })
+        //         }
+        //     })
+        // })
         
-        app.put('/api/removeFromBlacklist', (req, res)=>{
-            let {items} = req.body
-            app.get('db').get_blacklist([req.user.id]).then((response)=>{
-                 response = response[0]
-                if (response){
-                    let newList = response.split(',').map((e,i,a)=>{
-                        if (!items.includes(e)) return e
-                    })
-                    newList = newList.filter((e,i,a)=>{
-                    return e !== undefined
-                    })
-                    newList = newList.join(',')
-                    app.get('db').update_blacklist([newList]).then((response)=>{
-                        res.status(200).send(response)
-                    })
-                }
-            })
-        })
+        // app.put('/api/removeFromBlacklist', (req, res)=>{
+        //     let {items} = req.body
+        //     app.get('db').get_blacklist([req.user.id]).then((response)=>{
+        //          response = response[0]
+        //         if (response){
+        //             let newList = response.split(',').map((e,i,a)=>{
+        //                 if (!items.includes(e)) return e
+        //             })
+        //             newList = newList.filter((e,i,a)=>{
+        //             return e !== undefined
+        //             })
+        //             newList = newList.join(',')
+        //             app.get('db').update_blacklist([newList]).then((response)=>{
+        //                 res.status(200).send(response)
+        //             })
+        //         }
+        //     })
+        // })
+
 
         app.get('/api/getShoppingList', (req, res) => {
             app.get('db').get_shopping_list([req.user.id])
